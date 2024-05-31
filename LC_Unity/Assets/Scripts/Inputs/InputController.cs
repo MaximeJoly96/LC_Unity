85527b8ab98512acd5879822c598316e29e74b3e
Maxah <maxime.joly96@gmail.com>
2024-05-31 21:31:44 +0200

    Base class InputController for handling inputs

---
+++ LC_Unity/Assets/Scripts/Inputs/InputController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Inputs
+{
+    public class InputController : MonoBehaviour
+    {
+        // Update is called once per frame
+        private void Update()
+        {
+            HandleAxes();
+            HandleButtons();
+            HandleClicks();
+        }
+
+        private void HandleAxes()
+        {
+            if (Input.GetAxis("Horizontal") > 0.0f)
+            {
+                Debug.Log("going right");
+            }
+            else if (Input.GetAxis("Horizontal") < 0.0f)
+            {
+                Debug.Log("going left");
+            }
+
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                Debug.Log("going up");
+            }
+            else if (Input.GetAxis("Vertical") < 0.0f)
+            {
+                Debug.Log("going down");
+            }
+        }
+
+        private void HandleButtons()
+        {
+            if(Input.GetButtonDown("Select"))
+            {
+                Debug.Log("Pressed select");
+            }
+
+            if(Input.GetButtonDown("Cancel"))
+            {
+                Debug.Log("Pressed cancel");
+            }
+        }
+
+        private void HandleClicks()
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                Debug.Log("Clicked left");
+            }
+
+            if(Input.GetButtonDown("Fire2"))
+            {
+                Debug.Log("Clicked right");
+            }
+        }
+    }
+}
