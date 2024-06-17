ba8ffa21a655c013b43738dbc2d73de642f453d0
Maxah <maxime.joly96@gmail.com>
2024-06-17 14:27:31 +0200

    Huge cleanup

--- LC_Unity/Assets/Scripts/RPG_Maker_VX_Ace_Import/Database/Battlers/IAbilityBehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using RPG_Maker_VX_Ace_Import.Database.System;
-
-namespace RPG_Maker_VX_Ace_Import.Database.Battlers
-{
-    public interface IAbilityBehaviour
-    {
-        ElementalTypes Element { get; }
-    }
-}
