b84c31a81b38eca930b28e7fd2e6f3705a9cce23
Maxah <maxime.joly96@gmail.com>
2024-06-22 15:26:46 +0200

    On peut déplacer le curseur horizontal du main menu

---
+++ LC_Unity/Assets/Scripts/Menus/HorizontalMainMenuController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using Core;
+using Inputs;
+
+namespace Menus
+{
+    public class HorizontalMainMenuController : MonoBehaviour
+    {
+        private const float DELAY_BETWEEN_ACTIONS = 0.2f; // seconds
+
+        [SerializeField]
+        private SubMenuButton[] _subMenuButtons;
+
+        private InputController _inputController;
+        private int _cursorPosition;
+        private float _selectionDelay;
+        private bool _busy;
+
+        private void Start()
+        {
+            _inputController = FindObjectOfType<InputController>();
+            _inputController.ButtonClicked.AddListener(HandleInputs);
+            _cursorPosition = 0;
+            _selectionDelay = 0.0f;
+            _busy = false;
+
+            UpdateCursorPosition();
+        }
+
+        private void HandleInputs(InputAction input)
+        {
+            if(GlobalStateMachine.Instance.CurrentState == GlobalStateMachine.State.InMenu && !_busy)
+            {
+                switch(input)
+                {
+                    case InputAction.MoveLeft:
+                        MoveCursorLeft();
+                        break;
+                    case InputAction.MoveRight:
+                        MoveCursorRight();
+                        break;
+                    case InputAction.Select:
+                        SelectSubMenu();
+                        break;
+                }
+            }
+        }
+
+        private void MoveCursorLeft()
+        {
+            _cursorPosition = _cursorPosition == 0 ? _subMenuButtons.Length - 1 : --_cursorPosition;
+            UpdateCursorPosition();
+        }
+
+        private void MoveCursorRight()
+        {
+            _cursorPosition = _cursorPosition == _subMenuButtons.Length - 1 ? 0 : ++_cursorPosition;
+            UpdateCursorPosition();
+        }
+
+        private void UpdateCursorPosition()
+        {
+            for(int i = 0; i < _subMenuButtons.Length; i++)
+            {
+                _subMenuButtons[i].DisplayCursor(i == _cursorPosition);
+            }
+
+            _busy = true;
+        }
+
+        private void SelectSubMenu()
+        {
+
+        }
+
+        private void Update()
+        {
+            if(_busy)
+            {
+                _selectionDelay += Time.deltaTime;
+
+                if (_selectionDelay >= DELAY_BETWEEN_ACTIONS)
+                {
+                    _selectionDelay = 0.0f;
+                    _busy = false;
+                }
+            }
+        }
+    }
+}
---
+++ LC_Unity/Assets/Scripts/Menus/SubMenuButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menus
+{
+    public class SubMenuButton : MonoBehaviour
+    {
+        private Transform Background
+        {
+            get { return transform.Find("Bg"); }
+        }
+
+        private Transform Cursor
+        {
+            get { return transform.Find("CursorWrapper"); }
+        }
+
+        public void DisplayCursor(bool display)
+        {
+            Background.gameObject.SetActive(display);
+            Cursor.gameObject.SetActive(display);
+        }
+    }
+}
