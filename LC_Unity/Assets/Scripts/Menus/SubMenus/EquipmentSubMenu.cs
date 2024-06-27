afbaae84cf8d092ab47867bd8e65ee669f7acbd8
Maxah <maxime.joly96@gmail.com>
2024-06-27 13:22:16 +0200

    Le menu equipement est nourri avec les bonnes infos

--- LC_Unity/Assets/Scripts/Menus/SubMenus/EquipmentSubMenu.cs
+++ LC_Unity/Assets/Scripts/Menus/SubMenus/EquipmentSubMenu.cs
@@ -1,11 +1,39 @@
 ﻿using Core;
+using TMPro;
+using UnityEngine;
+using Menus.SubMenus.Items;
+using Menus.SubMenus.Status;
 
 namespace Menus.SubMenus
 {
     public class EquipmentSubMenu : SubMenu
     {
+        private int _cursorPosition;
+        private float _delay;
+
+        [SerializeField]
+        private TMP_Text _characterName;
+        [SerializeField]
+        private SelectableItemsList _itemsList;
+        [SerializeField]
+        private TMP_Text _currentItemDescription;
+        [SerializeField]
+        private StatsPanel _stats;
+        [SerializeField]
+        private EquipmentPanel _equipment;
+        [SerializeField]
+        private EffectsPanel _effects;
+
         public override void Open()
         {
+            _cursorPosition = 0;
+            _delay = 0.0f;
+
+            Init();
+
+            _itemsList.ItemHovered.RemoveAllListeners();
+            _itemsList.ItemHovered.AddListener(UpdateItemDescription);
+
             StartCoroutine(DoOpen());
             GlobalStateMachine.Instance.UpdateState(GlobalStateMachine.State.InMenuEquipmentTab);
         }
@@ -18,6 +46,21 @@ namespace Menus.SubMenus
         protected override void FinishedClosing()
         {
             GlobalStateMachine.Instance.UpdateState(GlobalStateMachine.State.SelectingCharacterPreview);
+        }
+
+        private void UpdateItemDescription(SelectableItem item)
+        {
+            _currentItemDescription.text = item.Item.Description;
+        }
+
+        private void Init()
+        {
+            if(_fedCharacter != null)
+            {
+                _characterName.text = _fedCharacter.Name;
+                _stats.Feed(_fedCharacter);
+                _equipment.Feed(_fedCharacter);
+            }
         }
     }
 }
