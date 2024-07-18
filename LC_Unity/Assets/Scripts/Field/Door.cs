c32e89f69454a2c681a7eec4406c312ae240d8ad
Maxah <maxime.joly96@gmail.com>
2024-07-18 20:40:57 +0200

    Bonne transition entre le rdc et la cave

--- LC_Unity/Assets/Scripts/Field/Door.cs
+++ LC_Unity/Assets/Scripts/Field/Door.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 using UnityEngine.Events;
 using MusicAndSounds;
 using Engine.MusicAndSounds;
+using System.Collections;
 
 namespace Field
 {
     public class Door : RunnableAgent
     {
+        public enum DoorType { ToInterior, ToOutside, ChangeFloor }
+
+        [SerializeField]
+        protected Transform _interiorToShow;
+        [SerializeField]
+        protected Transform _interiorToHide;
+        [SerializeField]
+        protected Vector3 _playerPositionShift;
+        [SerializeField]
+        protected Vector3 _playerScale;
+        [SerializeField]
+        protected DoorType _type;
+
         private UnityEvent<bool> _doorStatusChanged;
 
         public UnityEvent<bool> DoorStatusChanged
@@ -21,32 +35,66 @@ namespace Field
             }
         }
 
-        [SerializeField]
-        protected Transform _interior;
-
         public override void RunSequence()
         {
-            GetComponent<Animator>().Play("StandardDoorOpen");
-            FindObjectOfType<AudioPlayer>().PlaySoundEffect(new PlaySoundEffect
+            if(_type != DoorType.ChangeFloor)
             {
-                Name = "OpenWoodenDoor",
-                Volume = 0.25f,
-                Pitch = 1.0f
-            });
+                FindObjectOfType<AudioPlayer>().PlaySoundEffect(new PlaySoundEffect
+                {
+                    Name = "OpenWoodenDoor",
+                    Volume = 0.25f,
+                    Pitch = 1.0f
+                });
+            }
+
+            if (_type == DoorType.ToInterior)
+            {
+                Animator animator = GetComponent<Animator>();
+
+                if (animator)
+                    animator.Play("StandardDoorOpen");
+
+                return;
+            }
+
+            StartCoroutine(Exit());
+            
         }
 
         public virtual void FinishedOpening()
         {
-            _interior.gameObject.SetActive(true);
-
             PlayerController pc = FindObjectOfType<PlayerController>();
 
-            pc.GetComponent<SpriteRenderer>().sortingLayerName = "InteriorPlayer";
-            pc.transform.Translate(new Vector3(0.0f, 0.25f));
-            pc.transform.localScale = new Vector3(0.8f, 0.8f);
+            switch (_type)
+            {
+                case DoorType.ToInterior:
+                    _interiorToShow.gameObject.SetActive(true);
+                    pc.GetComponent<SpriteRenderer>().sortingLayerName = "InteriorPlayer";
+                    break;
+                case DoorType.ToOutside:
+                    _interiorToHide.gameObject.SetActive(false);
+                    pc.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
+                    FindObjectOfType<FieldBuilder>().SwitchToInteriorMode(false);
+                    break;
+                case DoorType.ChangeFloor:
+                    _interiorToHide.gameObject.SetActive(false);
+                    _interiorToShow.gameObject.SetActive(true);
+                    pc.GetComponent<SpriteRenderer>().sortingLayerName = "InteriorPlayer";
+                    break;
+            }
+
+            pc.transform.Translate(_playerPositionShift);
+            pc.transform.localScale = _playerScale;
 
             FinishedSequence.Invoke();
-            DoorStatusChanged.Invoke(true);
+            DoorStatusChanged.Invoke(_type != DoorType.ToOutside);
+        }
+
+        protected IEnumerator Exit()
+        {
+            yield return null;
+
+            FinishedOpening();
         }
     }
 }
