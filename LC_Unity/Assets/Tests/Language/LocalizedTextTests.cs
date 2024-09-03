758f601185e0ce81fdd566d334d29df0a4e34384
Maxah <maxime.joly96@gmail.com>
2024-09-03 15:47:52 +0200

    Test pour LocalizedText

---
+++ LC_Unity/Assets/Tests/Language/LocalizedTextTests.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using TMPro;
+using Language;
+using UnityEditor;
+
+namespace Testing.Languages
+{
+    public class LocalizedTextTests
+    {
+        [Test]
+        public void UpdateTextTest()
+        {
+            Setup();
+
+            GameObject go = new GameObject("Text");
+            TMP_Text text = go.AddComponent<TextMeshProUGUI>();
+
+            LocalizedText lt = go.AddComponent<LocalizedText>();
+            lt.UpdateKey("key1");
+
+            Assert.AreEqual("Valeur", text.text);
+
+            lt.UpdateKey("key2");
+
+            Assert.AreEqual("Autre valeur", text.text);
+        }
+
+        private void Setup()
+        {
+            GameObject go = new GameObject("Localizer");
+            Localizer localzier = go.AddComponent<Localizer>();
+
+            TextAsset file = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Tests/Resources/Languages/french.csv");
+            localzier.LoadLanguage(Language.Language.French, file);
+        }
+    }
+}
