c1fb0cd7f4af7662fe85dd410ed83b0f4265e728
Maxah <maxime.joly96@gmail.com>
2024-09-04 20:51:50 +0200

    Tests sur GlobalTimer et Waiter

---
+++ LC_Unity/Assets/Tests/Timing/GlobalTimerTests.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using System.Collections;
+using Timing;
+
+namespace Testing.Timing
+{
+    public class GlobalTimerTests
+    {
+        [UnityTest]
+        public IEnumerator TrackGlobalTimerTest()
+        {
+            GlobalTimer globalTimer = Setup();
+
+            Assert.IsTrue(Mathf.Abs(0.0f - globalTimer.InGameTimeSeconds) < 0.001f);
+
+            globalTimer.Running = true;
+
+            yield return null;
+
+            Assert.IsTrue(globalTimer.InGameTimeSeconds > 0.0f);
+        }
+
+        [UnityTest]
+        public IEnumerator InitInGameTimerTest()
+        {
+            GlobalTimer globalTimer = Setup();
+
+            globalTimer.InitInGameTimer(30.0f);
+
+            Assert.IsTrue(Mathf.Abs(30.0f - globalTimer.InGameTimeSeconds) < 0.001f);
+
+            globalTimer.Running = true;
+
+            yield return null;
+
+            Assert.IsTrue(globalTimer.InGameTimeSeconds > 30.0f);
+        }
+
+        [UnityTest]
+        public IEnumerator ChangeOfSecondIsDetectedTest()
+        {
+            GlobalTimer globalTimer = Setup();
+            globalTimer.Running = true;
+
+            globalTimer.GlobalTimeChangedEvent.AddListener((x) => Assert.IsTrue(Mathf.Abs(1.0f - globalTimer.InGameTimeSeconds) < 0.001f));
+
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        private GlobalTimer Setup()
+        {
+            GameObject timer = new GameObject("Timer");
+            GlobalTimer globalTimer = timer.AddComponent<GlobalTimer>();
+
+            return globalTimer;
+        }
+    }
+}
---
+++ LC_Unity/Assets/Tests/Timing/WaiterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Timing;
+using UnityEngine.TestTools;
+using System.Collections;
+using Engine.Timing;
+
+namespace Testing.Timing
+{
+    public class WaiterTests
+    {
+        [UnityTest]
+        public IEnumerator DetectWaitIsFinishedTest()
+        {
+            GameObject go = new GameObject("Waiter");
+            Waiter waiter = go.AddComponent<Waiter>();
+
+            Wait wait = new Wait { Duration = 1.0f };
+            wait.Finished.AddListener(() => Assert.IsTrue(wait.IsFinished));
+
+            waiter.Wait(wait);
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
