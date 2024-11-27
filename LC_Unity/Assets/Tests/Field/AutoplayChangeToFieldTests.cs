99019c8a3e0cead9e08b6a5f584ace56b1ed30e6
Maxah <maxime.joly96@gmail.com>
2024-11-27 22:04:08 +0100

    Ajout d'un test pour vérifier la nouvelle classe

---
+++ LC_Unity/Assets/Tests/Field/AutoplayChangeToFieldTests.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+using Engine.Events;
+using Field;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Testing.Field
+{
+    public class AutoplayChangeToFieldTests : TestFoundation
+    {
+        [UnityTest]
+        public IEnumerator GlobalStateChangesToOnFieldAfterInstantiation()
+        {
+            GlobalStateMachine.Instance.UpdateState(GlobalStateMachine.State.BrowsingQuests);
+
+            AutoplayChangeToField auto = CreateAutoplay();
+
+            yield return null;
+
+            Assert.AreEqual(GlobalStateMachine.State.OnField, GlobalStateMachine.Instance.CurrentState);
+        }
+
+        private AutoplayChangeToField CreateAutoplay()
+        {
+            GameObject go = ComponentCreator.CreateEmptyGameObject();
+            _usedGameObjects.Add(go);
+
+            go.AddComponent<EventsRunner>();
+            return go.AddComponent<AutoplayChangeToField>();
+        }
+    }
+}
